22289771ed2dbf76f1a29e8f96ed50332a50a642
Noel Pinto <noelpinto47@gmail.com>
2025-01-24 23:36:25 +0530

    made progress on the homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,33 @@
+import 'package:basic_underline/basic_underline.dart';
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio_website_noelpinto/pages/about.dart';
 import 'package:portfolio_website_noelpinto/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
   static List<String> menuOptions = ["Home", "About", "Projects", "Contact"];
+  static List<Widget> pages = [const HomePage(), const AboutPage()];
+  static int currentIndex = 0;
+
+  late SwiperController swiperController;
+
+  @override
+  void initState() {
+    super.initState();
+    swiperController = SwiperController();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,54 +41,84 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: Scaffold(
-          backgroundColor: Color(0xff252525),
-          appBar: AppBar(
-            title: Text(
-              'Noel Pinto',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: GoogleFonts.inter().fontFamily,
-                  fontWeight: FontWeight.bold),
-            ),
-            centerTitle: true,
-            backgroundColor: Color(0xff252525),
-            surfaceTintColor: Colors.transparent,
-            shape: Border(
-                bottom: BorderSide(color: Color(0xffC6A686), width: 1.0)),
-            toolbarHeight: height * 0.1,
-            leading: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: menuOptions.length,
-                itemBuilder: (context, index) {
-                  return TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      menuOptions[index],
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: GoogleFonts.inter().fontFamily,
-                        fontSize: 16,
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-            leadingWidth: height * 0.4,
-            actions: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.dark_mode, color: Colors.white)),
-              )
-            ],
+        backgroundColor: Color(0xff252525),
+        appBar: AppBar(
+          title: Text(
+            'Noel Pinto',
+            style: TextStyle(
+                color: Colors.white,
+                fontFamily: GoogleFonts.inter().fontFamily,
+                fontWeight: FontWeight.bold),
           ),
-          body: SingleChildScrollView(
-            child: HomePage(),
-          )),
+          centerTitle: true,
+          backgroundColor: Color(0xff252525),
+          surfaceTintColor: Colors.transparent,
+          shape:
+              Border(bottom: BorderSide(color: Color(0xffC6A686), width: 1.0)),
+          toolbarHeight: height * 0.1,
+          leading: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: menuOptions.length,
+              itemBuilder: (context, index) {
+                return TextButton(
+                  onPressed: () {
+                    setState(() {
+                      currentIndex = index;
+                      swiperController.move(index);
+                    });
+                  },
+                  style: ButtonStyle(
+                    overlayColor: WidgetStateProperty.all(Colors.transparent),
+                    mouseCursor: WidgetStatePropertyAll(MouseCursor.defer),
+                  ),
+                  child: UnderlineText(
+                    text: menuOptions[index],
+                    textColor: index == currentIndex
+                        ? Color(0xffC6A686)
+                        : Colors.white,
+                    underlineColor: Color(0xffC6A686),
+                    hoverTextColor: Color(0xffC6A686),
+                  ),
+                );
+              },
+            ),
+          ),
+          leadingWidth: height * 0.4,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.dark_mode, color: Colors.white)),
+            )
+          ],
+        ),
+        body: Swiper(
+          controller: swiperController,
+          itemCount: 2,
+          index: currentIndex,
+          itemBuilder: (context, index) {
+            return pages[index];
+          },
+          scrollDirection: Axis.vertical,
+          onIndexChanged: (value) {
+            setState(() {
+              currentIndex = value;
+            });
+          },
+          pagination: SwiperPagination(
+            alignment: Alignment.centerRight,
+            builder: DotSwiperPaginationBuilder(
+              color: Colors.white,
+              activeColor: Color(0xffC6A686),
+            ),
+          ),
+          curve: Curves.easeIn,
+          loop: false,
+        ),
+      ),
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,57 +7,97 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    final isPhone = width < 600;
 
     return Container(
       alignment: Alignment.center,
-      padding: EdgeInsets.all(80),
-      child: Column(children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              flex: 5,
-              child: Container(
-                height: 600,
-                decoration: BoxDecoration(
-                    border: Border.all(color: Color(0xffC6A686), width: 1)),
-                child: Image.asset(
-                  'assets/images/selfpic.jpg',
-                  fit: BoxFit.cover,
+      padding: EdgeInsets.all(40),
+      child: isPhone
+          ? Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 300,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Color(0xffC6A686), width: 1),
+                  ),
+                  child: Image.asset(
+                    'assets/images/selfpic.jpg',
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-            ),
-            Spacer(),
-            Expanded(
-              flex: 5,
-              child: Container(
+                SizedBox(height: height * 0.02),
+                Container(
                   alignment: Alignment.centerLeft,
-                  height: 600,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    spacing: height * 0.02,
                     children: [
                       Text(
                         'Hello, I am Noel Pinto.',
                         style: TextStyle(
                             fontFamily: GoogleFonts.inter().fontFamily,
-                            fontSize: 40,
+                            fontSize: 30,
                             color: Colors.white),
                       ),
                       Text(
                           'A Mobile Application Developer\nusing Flutter and Native Android/iOS\nbased in Mumbai.',
                           style: TextStyle(
                               fontFamily: GoogleFonts.inter().fontFamily,
-                              fontSize: 40,
+                              fontSize: 30,
                               color: Colors.white,
                               fontWeight: FontWeight.bold)),
                     ],
-                  )),
+                  ),
+                ),
+              ],
+            )
+          : Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Container(
+                    height: 600,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color(0xffC6A686), width: 1),
+                    ),
+                    child: Image.asset(
+                      'assets/images/selfpic.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Spacer(),
+                Expanded(
+                  flex: 5,
+                  child: Container(
+                      alignment: Alignment.centerLeft,
+                      height: 600,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Hello, I am Noel Pinto.',
+                            style: TextStyle(
+                                fontFamily: GoogleFonts.inter().fontFamily,
+                                fontSize: 40,
+                                color: Colors.white),
+                          ),
+                          Text(
+                              'A Mobile Application Developer\nusing Flutter and Native Android/iOS\nbased in Mumbai.',
+                              style: TextStyle(
+                                  fontFamily: GoogleFonts.inter().fontFamily,
+                                  fontSize: 40,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                        ],
+                      )),
+                ),
+              ],
             ),
-          ],
-        ),
-      ]),
     );
   }
 }
